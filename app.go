c59647947d3dc5e743805ea08edf63eced981eaa
pengfei.lv <pengfei.lv@ucloud.cn>
2019-11-17 00:52:34 +0800

    main注释

--- app.go
+++ app.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"gin-gorm-demo/database"
 	"gin-gorm-demo/http"
 	"gin-gorm-demo/models"
 )
 
-//_ "github.com/jinzhu/gorm/dialects/mysql"
-
 func main() {
-	//var err error
-
+	// 数据库初始化
 	database.InitMysql()
+	// main函数执行结束后，关闭数据库连接，这里为了方便调用，我直接在main函数中关闭
+	// 如果需要每次调用的收连接数据库，可以将该方法定义接口，在函数中调用，关闭
 	defer database.MYSQLDB.Close()
-
+	// 数据库迁移
 	models.AutoMigrateDB()
-
-	//mysql_conf := con.Mysql
-	//connect_sql := mysql_conf.Username + ":" + mysql_conf.Password + "@tcp(" + mysql_conf.Addr + ":" + mysql_conf.Port + ")/" + mysql_conf.Database + "?"
-	//database.MYSQLDB, err = gorm.Open("mysql", connect_sql+"charset=utf8&parseTime=True&loc=Local")
-	//database.MYSQLDB.SingularTable(true) // User表表明默认为users,  如果设置了这一句，创建的表为user, 而不是用复数
-
-	//gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-	//return "prefix_" + defaultTableName
-	//}
-
-	//if err != nil {
-	//fmt.Println("connection err")
-	//} else {
-	//switch con.Env {
-	//case "development":
-	//fmt.Println("current environment is development")
-	//case "production":
-	//fmt.Println("current environment is production")
-	//case "test":
-	//fmt.Println("current environment is test")
-	//}
-	//}
-	//defer database.MYSQLDB.Close()
-	//models.InitDB()
-
+	// 获取配置文件的监听端口
 	var con database.BuildConf
 	con.GetConf()
-
-	fmt.Println("con", con)
+	// 初始化router
 	router := http.InitRouter()
 	router.Run("0.0.0.0:" + con.Listen)
 }
